6fccb1c94699e8cf98140f42be234596b9dd6b91
Anastasia Shatokhina <shnatya@gmail.com>
2021-08-23 22:29:51 -0700

    commit tree bca0e2b595a716c9fbdb7513f3b974937630007a

--- index.js
+++ index.js
@@ -11,6 +11,31 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+//This function  return a new array with all the titles is in "title case",
+//in other words, the first letter of each word is capitalized. 
+
+/* const titleCased = () => {
+  return tutorials.map(item => {
+   
+    const newStringArray = item.split(' '); // a string converts to an array with elements-words
+    let str = '';
+    for(const element of newStringArray) { //every element-word in the array got capitalized
+       str += element.charAt(0).toUpperCase() + element.slice(1) + " "; // take the first letter and capitalize + the rest of the word using slice
+     
+   }
+    return str.slice(0, (str.length - 1)); //get rid of the last space in the sentence
+})
+ 
+}
+*/
+
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(item => {
+   
+    const newStringArray = item.split(' '); // a string converts to an array with elements-words
+	  return newStringArray.map(word => {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+    }).join(" ");
+})
+ 
 }
